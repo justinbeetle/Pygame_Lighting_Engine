a636fb1389b5a052ae3d7f5fd019a3bb867bb150
justinbeetle <30003866+justinbeetle@users.noreply.github.com>
2026-01-01 14:13:08 -0600

    Work-in-progress to add typing.

--- Dungeon_Example_Project/Light_Engine.py
+++ Dungeon_Example_Project/Light_Engine.py
@@ -2,9 +2,10 @@
 
 import copy
 import math as meth
-import numpy as np
 import random
+from typing import NamedTuple, Optional
 
+import numpy as np
 import pygame
 
 pygame.init()
@@ -15,7 +16,7 @@ scale_factor = 3
 screen_size_pixels = display_size_pixels[0] * scale_factor, display_size_pixels[1] * scale_factor
 screen = pygame.display.set_mode(screen_size_pixels, pygame.DOUBLEBUF)
 display = pygame.Surface(display_size_pixels)
-clock, fps = pygame.time.Clock(), 30
+clock, fps = pygame.time.Clock(), 1000
 
 tileset = pygame.transform.scale(pygame.image.load("Dungeon_Tileset.png"), (160, 160)).convert_alpha()
 tile_size_px = 16
@@ -28,17 +29,17 @@ def get_tile(x: int, y: int) -> pygame.surface.Surface:
     return img.copy()
 
 
-tiles_textures = []
+tile_textures = []
 
 for y in range(10):
     for x in range(10):
-        tiles_textures.append(get_tile(x, y))
+        tile_textures.append(get_tile(x, y))
 
 
-class LIGHT:
+class Light:
     def __init__(
         self,
-        size: int,
+        radius_px: int,
         color: pygame.Color,
         intensity: float,
         is_point: bool,
@@ -48,22 +49,22 @@ class LIGHT:
         """
         Docstring for __init__
 
-        :param size: The diameter of the light
+        :param radius_px: The radius of the light in pixels
         :param color: Color of the light
         :param intensity: Intensity of the light [0.0, 1.0]
         :param is_point: If the light is a point (directional) light source
         :param angle_deg: For a point light, the direction of the light's beam in degrees [0.0, 360.0]
         :param angle_width_deg: For a point light, the width of the lights's bean in degrees [0.0, 360.0]
         """
-        self.size = size
-        self.radius = size * 0.5
-        self.render_surface = pygame.Surface((size, size))
+        self.size_px = radius_px * 2
+        self.radius_px = radius_px
+        self.render_surface = pygame.Surface((self.size_px, self.size_px))
         self.intensity = intensity
         self.angle = angle_deg
         self.angle_width = angle_width_deg
         self.point = is_point
         self.pixel_shader_surf = self.pixel_shader(
-            np.full((size, size, 3), (color.r, color.g, color.b), dtype=np.uint8)
+            np.full((self.size_px, self.size_px, 3), (color.r, color.g, color.b), dtype=np.uint8)
         )
         self.render_surface.set_colorkey((0, 0, 0))
 
@@ -72,67 +73,67 @@ class LIGHT:
         dy = p2[1] - p1[1]
 
         if dx == 0:
-            return [p2[0], (0 if dy <= 0 else self.size)]
+            return [p2[0], (0 if dy <= 0 else self.size_px)]
 
         if dy == 0:
-            return [(0 if dx <= 0 else self.size), p2[1]]
+            return [(0 if dx <= 0 else self.size_px), p2[1]]
 
         y_gradient = dy / dx
         y_intercept = p1[1] - (p1[0] * y_gradient)
 
-        y_line = 0 if dx <= 0 else self.size
+        y_line = 0 if dx <= 0 else self.size_px
         y_intersection = [y_line, (y_gradient * y_line) + y_intercept]
 
-        if y_intersection[1] >= 0 and y_intersection[1] <= self.size:
+        if y_intersection[1] >= 0 and y_intersection[1] <= self.size_px:
             return y_intersection
 
         x_gradient = dx / dy
         x_intercept = p1[0] - (p1[1] * x_gradient)
 
-        x_line = 0 if dy <= 0 else self.size
+        x_line = 0 if dy <= 0 else self.size_px
         x_intersection = [(x_gradient * x_line) + x_intercept, x_line]
 
-        if x_intersection[0] >= 0 and x_intersection[0] <= self.size:
+        if x_intersection[0] >= 0 and x_intersection[0] <= self.size_px:
             return x_intersection
 
     def fill_shadows(self, render_surface: pygame.surface.Surface, points) -> None:
         render_points = [points[0], points[4], points[1], points[2], points[3]]
 
         if points[2][0] + points[3][0] not in [1000, 0] and points[2][1] + points[3][1] not in [1000, 0]:
-            if abs(points[2][0] - points[3][0]) == self.size:  # x opposite
+            if abs(points[2][0] - points[3][0]) == self.size_px:  # x opposite
 
-                if self.radius < points[2][1]:
+                if self.radius_px < points[2][1]:
                     render_points = [
                         points[0],
                         points[4],
                         points[1],
                         points[2],
-                        [0, self.size],
-                        [self.size, self.size],
+                        [0, self.size_px],
+                        [self.size_px, self.size_px],
                         points[3],
                     ]
 
-                if self.radius > points[2][1]:
-                    render_points = [points[0], points[4], points[1], points[2], [self.size, 0], [0, 0], points[3]]
+                if self.radius_px > points[2][1]:
+                    render_points = [points[0], points[4], points[1], points[2], [self.size_px, 0], [0, 0], points[3]]
 
-            elif abs(points[2][1] - points[3][1]) == self.size:  # y opposite
+            elif abs(points[2][1] - points[3][1]) == self.size_px:  # y opposite
 
-                if self.radius < points[2][0]:
+                if self.radius_px < points[2][0]:
                     render_points = [
                         points[0],
                         points[4],
                         points[1],
                         points[2],
-                        [self.size, self.size],
-                        [self.size, 0],
+                        [self.size_px, self.size_px],
+                        [self.size_px, 0],
                         points[3],
                     ]
 
-                if self.radius > points[2][0]:
-                    render_points = [points[0], points[4], points[1], points[2], [0, self.size], [0, 0], points[3]]
+                if self.radius_px > points[2][0]:
+                    render_points = [points[0], points[4], points[1], points[2], [0, self.size_px], [0, 0], points[3]]
 
             else:
-                if points[2][0] != self.size and points[2][0] != 0:
+                if points[2][0] != self.size_px and points[2][0] != 0:
                     render_points = [
                         points[0],
                         points[4],
@@ -181,7 +182,7 @@ class LIGHT:
 
         return corners
 
-    def get_tiles(self, tiles, mx, my):
+    def get_tiles(self, tiles: list[list[int]], x_px: int, y_px: int) -> list[list]:
         points = []
 
         h = len(tiles)
@@ -190,9 +191,11 @@ class LIGHT:
             for x in range(w):
                 if tiles[y][x]:
                     if (
-                        x * tile_size_px - mx >= (-self.radius) - tile_size_px and x * tile_size_px - mx <= self.radius
+                        x * tile_size_px - x_px >= (-self.radius_px) - tile_size_px
+                        and x * tile_size_px - x_px <= self.radius_px
                     ) and (
-                        y * tile_size_px - my >= (-self.radius) - tile_size_px and y * tile_size_px - my <= self.radius
+                        y * tile_size_px - y_px >= (-self.radius_px) - tile_size_px
+                        and y * tile_size_px - y_px <= self.radius_px
                     ):
                         points.append(
                             [
@@ -213,16 +216,16 @@ class LIGHT:
             for y in range(len(final_array[x])):
 
                 # radial -----
-                distance = meth.sqrt((x - self.radius) ** 2 + (y - self.radius) ** 2)
+                distance = meth.sqrt((x - self.radius_px) ** 2 + (y - self.radius_px) ** 2)
 
-                radial_falloff = (self.radius - distance) * (1 / self.radius)
+                radial_falloff = (self.radius_px - distance) * (1 / self.radius_px)
 
                 if radial_falloff <= 0:
                     radial_falloff = 0
                 # -----
 
                 # angular -----
-                point_angle = (180 / meth.pi) * -meth.atan2((self.radius - x), (self.radius - y)) + 180
+                point_angle = (180 / meth.pi) * -meth.atan2((self.radius_px - x), (self.radius_px - y)) + 180
                 diff_anlge = abs(((self.angle - point_angle) + 180) % 360 - 180)
 
                 angular_falloff = ((self.angle_width / 2) - diff_anlge) * (1 / self.angle_width)
@@ -258,17 +261,20 @@ class LIGHT:
 
         return render
 
-    def main(self, tiles, display: pygame.surface.Surface, mx: int, my: int) -> pygame.surface.Surface:
+    def add_light(
+        self, light_surface: pygame.surface.Surface, shadow_tiles: list[list[int]], x_px: int, y_px: int
+    ) -> None:
+
         self.render_surface.fill((0, 0, 0))
         self.render_surface.blit(self.pixel_shader_surf, (0, 0))
 
-        dx, dy = mx - self.radius, my - self.radius
+        dx, dy = x_px - self.radius_px, y_px - self.radius_px
 
-        for point in self.get_tiles(tiles, mx, my):
+        for point in self.get_tiles(shadow_tiles, x_px, y_px):
 
             if self.check_cast(point, dx, dy):
 
-                corners = self.get_corners(point, mx, my)
+                corners = self.get_corners(point, x_px, y_px)
                 corners = [
                     [corners[0][0] - dx, corners[0][1] - dy],
                     [corners[1][0] - dx, corners[1][1] - dy],
@@ -279,21 +285,19 @@ class LIGHT:
                     [
                         corners[0],
                         corners[1],
-                        self.get_intersection([self.radius] * 2, corners[1]),
-                        self.get_intersection([self.radius] * 2, corners[0]),
+                        self.get_intersection([self.radius_px] * 2, corners[1]),
+                        self.get_intersection([self.radius_px] * 2, corners[0]),
                         corners[2],
                     ],
                 )
 
-        pygame.draw.circle(self.render_surface, (255, 255, 255), (self.radius, self.radius), 2)
+        pygame.draw.circle(self.render_surface, (255, 255, 255), (self.radius_px, self.radius_px), 2)
 
-        display.blit(self.render_surface, (mx - self.radius, my - self.radius), special_flags=pygame.BLEND_RGBA_ADD)
-
-        return display
+        light_surface.blit(self.render_surface, (dx, dy), special_flags=pygame.BLEND_RGBA_ADD)
 
 
-class MAP:
-    def __init__(self):
+class Map:
+    def __init__(self) -> None:
         # 1 indicates a wall
         self.tiles = [
             [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
@@ -313,48 +317,48 @@ class MAP:
             [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
         ]
 
+        # Same si
         self.shadow_tiles = copy.deepcopy(self.tiles)
 
+        # Index in tile_textures for each tile
         self.texture_map = copy.deepcopy(self.tiles)
 
         self.generate_tiles()
 
     def render(self, win: pygame.surface.Surface) -> None:
+        """Blit all of the tiles in the map to the provided surface."""
         h = len(self.tiles)
         w = len(self.tiles[0])
         for y in range(h):
             for x in range(w):
                 tile_pos = [x * tile_size_px, y * tile_size_px]
-                win.blit(tiles_textures[self.texture_map[y][x]], tile_pos)
+                win.blit(tile_textures[self.texture_map[y][x]], tile_pos)
 
     def generate_tiles(self) -> None:
+        """Generate self.shadow_tiles and self.texture_map based on self.tiles."""
         h = len(self.tiles)
         w = len(self.tiles[0])
+        max_y = h - 1
         for y in range(h):
             for x in range(w):
-                self.texture_map[y][x] = self.render_tile(x, y, w, h)
-                if self.tiles[y][x]:
-                    if y == h - 1:
-                        self.shadow_tiles[y][x] = 1
-                    else:
-                        if not self.tiles[y + 1][x]:
-                            self.shadow_tiles[y][x] = 0
-                        else:
-                            self.shadow_tiles[y][x] = 1
-                else:
-                    self.shadow_tiles[y][x] = 0
+                self.texture_map[y][x] = self.select_tile_textures_index(x, y)
+                self.shadow_tiles[y][x] = self.tiles[y][x] and (y == max_y or self.tiles[y + 1][x])
 
-    def render_tile(self, x: int, y: int, w: int, h: int) -> None:
+    def select_tile_textures_index(self, x: int, y: int) -> int:
+        """Select an index in tile_textures at random based on which tiles in the local 3x3 block are walls.
+        NOTE: This method hardcodes knowledge about tile positions in Dungeon_Tileset.png."""
+        max_x = len(self.tiles[0]) - 1
+        max_y = len(self.tiles) - 1
         tile_states = [
             self.tiles[y - 1][x - 1] if x > 0 and y > 0 else 1,
             self.tiles[y - 1][x] if y > 0 else 1,
-            self.tiles[y - 1][x + 1] if x < w - 1 and y > 0 else 1,
+            self.tiles[y - 1][x + 1] if x < max_x and y > 0 else 1,
             self.tiles[y][x - 1] if x > 0 else 1,
             self.tiles[y][x],
-            self.tiles[y][x + 1] if x < w - 1 else 1,
-            self.tiles[y + 1][x - 1] if x > 0 and y < h - 1 else 1,
-            self.tiles[y + 1][x] if y < h - 1 else 1,
-            self.tiles[y + 1][x + 1] if x < w - 1 and y < h - 1 else 1,
+            self.tiles[y][x + 1] if x < max_x else 1,
+            self.tiles[y + 1][x - 1] if x > 0 and y < max_y else 1,
+            self.tiles[y + 1][x] if y < max_y else 1,
+            self.tiles[y + 1][x + 1] if x < max_x and y < max_y else 1,
         ]
 
         # --------------------------------------
@@ -390,67 +394,83 @@ class MAP:
         else:
             return random.choice([6, 7, 8, 9, 16, 17, 18, 19, 26, 27, 28, 29])
 
-    def clicking(self, mx, my, button):
-        if button:
-            self.tiles[my // tile_size_px][mx // tile_size_px] = 1
-        else:
-            self.tiles[my // tile_size_px][mx // tile_size_px] = 0
+    def clicking(self, x_px: int, y_px: int, place_wall: bool) -> None:
+        """Add or remove a wall tile based on pixel coordinates"""
+        self.tiles[y_px // tile_size_px][x_px // tile_size_px] = 1 if place_wall else 0
         self.generate_tiles()
 
 
-def global_light(size, intensity: float) -> pygame.surface.Surface:
-    dark = pygame.Surface(size).convert_alpha()
-    dark.fill((255, 255, 255, intensity))
-    return dark
+def add_ambient_light(
+    light_surface: pygame.surface.Surface, color: Optional[pygame.Color] = None, intensity: int = 50
+) -> None:
+    """Add ambient light of the specified color and intensity
+    :param intensity: [0, 255]
+    """
+    if color is None:
+        color = pygame.Color("white")
+    ambient_light = pygame.Surface(light_surface.get_size()).convert_alpha()
+    ambient_light.fill((color.r, color.g, color.b, intensity))
+    light_surface.blit(ambient_light)
 
 
-world = MAP()
+world = Map()
 
 # size, color, intensity, point, angle=0, angle_width=360
-light_test = LIGHT(150, pygame.Color(255, 185, 9), 1, False)
+mouse_light = Light(75, pygame.Color(255, 185, 9), 1, False)
 
-lights = []
 
-surfaces = []
+class MapLight(NamedTuple):
+    light: Light
+    x_px: int
+    y_px: int
+
+
+# List of tuples of lights and their positions
+map_lights: list[MapLight] = []
+
+
+def screen_to_display_coords(pos_px: tuple[int, int]) -> tuple[int, int]:
+    """Account for display to screen scaling"""
+    return pos_px[0] // scale_factor, pos_px[1] // scale_factor
+
 
 while True:
     display.fill((0, 0, 0))
-
-    mx, my = pygame.mouse.get_pos()
-    mx = round(mx // scale_factor)
-    my = round(my // scale_factor)
 
     world.render(display)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
-        if event.type == pygame.MOUSEBUTTONDOWN:
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_x_px, mouse_y_px = screen_to_display_coords(event.pos)
             if event.button == 1:  # left mouse button
                 # Add a wall on a left mouse click
-                world.clicking(event.pos[0] // scale_factor, event.pos[1] // scale_factor, 1)
-            if event.button == 2:  # mousewheel
+                world.clicking(mouse_x_px, mouse_y_px, place_wall=True)
+            elif event.button == 2:  # mousewheel
                 # Add a light on a mousewheel click
-                lights.append(
-                    [
-                        LIGHT(150, pygame.Color(255, 185, 9), 1, False),
-                        (event.pos[0] // scale_factor, event.pos[1] // scale_factor),
-                    ]
+                map_lights.append(
+                    MapLight(
+                        Light(75, pygame.Color(255, 185, 9), 1, False),
+                        mouse_x_px,
+                        mouse_y_px,
+                    )
                 )
-            if event.button == 3:  # right mouse button
+            elif event.button == 3:  # right mouse button
                 # Remove a wall on a left mouse click
-                world.clicking(event.pos[0] // scale_factor, event.pos[1] // scale_factor, 0)
+                world.clicking(mouse_x_px, mouse_y_px, place_wall=False)
 
-    lights_display = pygame.Surface((display.get_size()))
+    light_surface = pygame.Surface((display.get_size()))
+    add_ambient_light(light_surface)
 
-    lights_display.blit(global_light(display.get_size(), 50), (0, 0))
-    light_test.main(world.shadow_tiles, lights_display, mx, my)
+    mouse_x_px, mouse_y_px = screen_to_display_coords(pygame.mouse.get_pos())
+    mouse_light.add_light(light_surface, world.shadow_tiles, mouse_x_px, mouse_y_px)
 
-    for light in lights:
-        light[0].main(world.shadow_tiles, lights_display, light[1][0], light[1][1])
-        display.blit(tiles_textures[90], (light[1][0] - 8, light[1][1] - 8))
+    for map_light in map_lights:
+        map_light.light.add_light(light_surface, world.shadow_tiles, map_light.x_px, map_light.y_px)
+        display.blit(tile_textures[90], (map_light.x_px - tile_size_px // 2, map_light.y_px - tile_size_px // 2))
 
-    display.blit(lights_display, (0, 0), special_flags=pygame.BLEND_RGBA_MULT)
+    display.blit(light_surface, (0, 0), special_flags=pygame.BLEND_RGBA_MULT)
 
     surf = pygame.transform.scale(display, screen_size_pixels)
     screen.blit(surf, (0, 0))
